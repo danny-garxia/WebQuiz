510c0ff73ec6192c625614f5782f7998f4f7abbd
Nguyen Nguyen <nltn@msn.com>
2024-04-02 11:13:14 -0700

    Added navigation bar and user account area

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,44 @@
+function loadUserAccountArea() {
+	fetch('components/loginarea.html')
+		.then(response => response.text())
+		.then(html => {
+			document.getElementById('UserAccountArea').innerHTML = html;
+	});
+}
+
+function loadNavBar() {
+	fetch('components/navbar.html')
+		.then(response => response.text())
+		.then(html => {
+				document.getElementById('NavBar').innerHTML = html;
+		}).then(()=>{
+			loadUserAccountArea()
+	});
+}
+
+function loadHomePage() {
+	fetch('pages/home.html')
+		.then(response => response.text())
+		.then(html => {
+				document.getElementById('ContentArea').innerHTML = html;
+		}).then(()=>{
+			loadUserAccountArea()
+	});
+}
+
 (function (window) {
-    'use strict';
-    var App = window.App;
-    var UserManager = App.UserManager;
+		'use strict';
+
+		var USER_ACCOUNT_SELECTOR = '.UserAccount';
+
+		var App = window.App;
+		var UserManager = App.UserManager;
+
+		$(document).ready(function () {
+			loadNavBar();
+			loadHomePage();
+		});
+		
+		// var elem = document.querySelector(USER_ACCOUNT_SELECTOR);
+		// // elem.innerHTML = "login | signup";
 })(window);
